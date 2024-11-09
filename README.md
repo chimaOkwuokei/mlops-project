c508d32cfee5e1c06592a66c97d4bb4c5ae6ba13
unknown <chima.okwuokei@pau.edu.ng>
2024-11-09 11:01:28 +0100

    wine-quality-mlops

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mlops-project
-The first mlops project i've made
+
+# to create a virtual environment
+conda create -n mlproj
+
+# incase the powershell hasn't been initialized to use conda
+conda init powershell
+
+# to deactivate venv
+conda deactivate
+
+# Run these on terminal to set environment variables
+ $env:MLFLOW_TRACKING_URI = "https://dagshub.com/chimaOkwuokei/mlops-project.mlflow"
+ $env:MLFLOW_TRACKING_USERNAME = "chimaOkwuokei"        
+ $env:MLFLOW_TRACKING_PASSWORD = "aaffcd86f8b97bfe6e1e606aaee57ca5cd3512dc"
--- src/mlProject/pipeline/stage_05_model_evaluation.py
+++ src/mlProject/pipeline/stage_05_model_evaluation.py
@@ -9,10 +9,13 @@ class ModelEvaluationTrainingPipeline:
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        model_evaluation_config = config.get_model_evaluation_config()
-        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
-        model_evaluation_config.log_into_mlflow()
+        try:
+            config = ConfigurationManager()
+            model_evaluation_config = config.get_model_evaluation_config()
+            model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+            model_evaluation_config.log_into_mlflow()
+        except Exception as e:
+            raise e
 
 
 
